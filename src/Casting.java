d89487415bc0bf54cbcff6981b5c098c79280e6f
Nicollas <nsilvademoraes@gmail.com>
2024-06-02 14:20:23 -0300

    casting

---
+++ src/Casting.java
@@ -0,0 +1,24 @@
+public class Casting {
+    public static void main(String[] args) {
+
+        /*
+        Casting feito implicitamente, pois o valor possui um
+        tamanho menor que o tipo da varaivel que irá recebe-lo.
+         */
+
+//        int d = (int) 5.1974F;
+//        System.out.println(d);
+
+
+        float valorFloat = 3.14F;
+        int valorInt = (int) valorFloat;
+        float novoFloat = (float) valorInt;
+        novoFloat = 3.14F;
+
+        System.out.println(valorFloat);
+        System.out.println(valorInt);
+        System.out.println(novoFloat);
+        float novoValorFloat = (float) valorInt;
+        novoValorFloat = 3.14F;
+    }
+}
