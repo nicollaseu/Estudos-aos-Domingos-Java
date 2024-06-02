309cfd44d0e16613f040602d0a67d2dd92bd4083
Nicollas <nsilvademoraes@gmail.com>
2024-06-02 13:53:44 -0300

    Variaveis java

---
+++ src/Variaveis.java
@@ -0,0 +1,28 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        boolean casadoVerdadeiro = true;
+        boolean solteiroFalso = false;
+        char caracterUnico = 'A';
+        char caracterUnicode = '\u0041';
+        String nomeAluno = "Nicollas";
+        byte minhaIdade = 127;
+        short idadeCidade = 32767;
+        int idadeCidade2 =  324789809;
+        long numeroLong = 3247898090L;
+        float numeroPontoFlutuante = 12.4F;
+        double numeroDouble = 4551524619684.574;
+
+        System.out.println("true: " + casadoVerdadeiro);
+        System.out.println("Solteiro: " + solteiroFalso);
+        System.out.println("Caracter Unico:" + caracterUnico);
+        System.out.println("Unicode: " + caracterUnicode);
+        System.out.println("Nome: " + nomeAluno);
+        System.out.println("Idade: " + minhaIdade);
+        System.out.println("Idade Cidade: " + idadeCidade);
+        System.out.println("Idade Cidade 2: " + idadeCidade2);
+        System.out.println("Numero Long: " + numeroLong);
+        System.out.println("Numero Ponto Flutuante: " + numeroPontoFlutuante);
+        System.out.println("Numero Double: " + numeroDouble);
+
+    }
+}
